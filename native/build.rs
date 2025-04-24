68ed54b83b0a8795e927cdb377c0f02c077d6394
[name] <54875477+trance-mode@users.noreply.github.com>
2025-04-25 01:52:56 +0900

    add : アンインストーラー追加

--- native/build.rs
+++ native/build.rs
@@ -1,4 +1,5 @@
-// build.rs
 fn main() {
+    println!("cargo:rerun-if-changed=icon.ico");
+    println!("cargo:rerun-if-changed=icon.rc");
     embed_resource::compile("icon.rc", embed_resource::NONE);
 }
